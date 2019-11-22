10174fe224a046f987a91749a0862bc1a6487154
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-11-21 23:12:22 -0500

    done

--- react/FavoriteFoods.js
+++ react/FavoriteFoods.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
 export default class FavoriteFoods extends React.Component {
-
-  render () {
-
+  render() {
     return (
-
       <div>
         <h1>Favorite Foods</h1>
         <ul id="food-list">
+          {this.props.foods.map(food => {
+            return <li key={food.id}>{food.name}</li>;
+          })}
         </ul>
       </div>
-
     );
-
   }
-
 }
--- react/Homepage.js
+++ react/Homepage.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Profile from './Profile';
+import FavoriteFoods from './FavoriteFoods';
 
 export default class Homepage extends React.Component {
-
-  render () {
-
-    return (
-
-      <div id="clubhouse-homepage">
-      </div>
-
-    );
-
+  constructor() {
+    super();
+    this.state = { favoriteFoods: [], pugProfile: {} };
   }
-
+  render() {
+    return (
+      <div id="clubhouse-homepage">
+        <Profile profile={this.state.pugProfile} />
+        <FavoriteFoods foods={this.state.favoriteFoods} />
+      </div>
+    );
+  }
 }
